ba4d6e251d25ec0aed344fcfb143daab22fe196b
rodrigojgq <rodrigojgq@gmail.com>
2022-02-14 19:13:56 -0400

    ✨[ADD] Checkout Page

--- src/components/OrderItem.jsx
+++ src/components/OrderItem.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Image from 'next/image';
 import AppContext from '../context/AppContext';
 import close from '@icons/icon_close.png';
 import styles from '@styles/OrderItem.module.scss';
@@ -12,11 +13,11 @@ const OrderItem = ({ product }) => {
   return (
     <div className={styles.OrderItem}>
         <figure>
-          <img src={product.images[0]} alt={product.title}/>
+          <img src={product?.images[0]} alt={product?.title}/>
         </figure>
-        <p>{product.title}</p>
-        <p>${product.price}</p>
-        <img src={close} alt="close" onClick={() => handleRemove(product)}/>
+        <p>{product?.title}</p>
+        <p>${product?.price}</p>
+        <Image src={close} alt="close" onClick={() => handleRemove(product)}/>
       </div>
   );
 };
